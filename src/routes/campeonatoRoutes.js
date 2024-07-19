90cd1be36d3d922e5097d623b18f77b2227c01ee
IcaroWil <icarofaria11@gmail.com>
2024-07-19 11:35:22 -0300

    feat: definir rotas para inserção de times e simulação de jogos

--- src/routes/campeonatoRoutes.js
+++ src/routes/campeonatoRoutes.js
@@ -5,4 +5,4 @@ const campeonatoController = require('../controllers/campeonatoController')
 router.post('/inserir-times', campeonatoController.inserirTimes)
 router.post('/simular-jogo', campeonatoController.simularJogo)
 
-module.exports = router;
+module.exports = router

35d15ab31645166a6069dac260afbf83bc3de833
IcaroWil <icarofaria11@gmail.com>
2024-07-19 16:11:46 -0300

    feat: configurando os testes para simular o jogo e atualizar pontuações

--- src/controllers/campeonatoController.js
+++ src/controllers/campeonatoController.js
@@ -1,10 +1,10 @@
 const { response } = require('express')
 const db = require('../models/db')
-const axios = require('axios')
+
 
 exports.inserirTimes = (req, res) => {
     const { times } = req.body
-    if (times.length !== 0) {
+    if (times.length !== 8) {
         return res.status(400).json({ message: 'Devem ser inseridos exatamente 8 times' })
     }
 
@@ -19,23 +19,16 @@ exports.inserirTimes = (req, res) => {
 exports.simularJogo = (req, res) => {
     const { time1_id, time2_id, fase } = req.body
 
-    axios.get('http://localhost:5000/simularJogo')
-        .then(response => {
-            const [gol_time1, gol_time2,] = response.data
+    const gol_time1 = Math.floor(Math.random() * 8)
+    const gol_time2 = Math.floor(Math.random() * 8)
 
-            const sql = 'INSERT INTO jogos (time1_id, time2_id, gol_time1, gol_time2, fase) VALUES (?, ?, ?, ?, ?)'
-            db.query(sql, [time1_id, time2_id, gol_time1, gol_time2, fase], (err, result) => {
-                if (err) throw err
+    const sql = 'INSERT INTO jogos (time1_id, time2_id, gol_time1, gol_time2, fase) VALUES (?, ?, ?, ?, ?)'
+    db.query(sql, [time1_id, time2_id, gol_time1, gol_time2, fase], (err, result) => {
+        if (err) throw err
 
-                db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time1, time1_id])
-                db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time2, time1_id])
-                db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time2, time2_id])
-                db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time1, time2_id])
+        db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time1, time1_id])
+        db.query('UPDATE times SET pontuacao = pontuacao + ? WHERE id = ?', [gol_time2, time2_id])
 
-                res.status(201).json({ message: 'Jogo simulado com sucesso!', gol_time1, gol_time2})
-            })
-        })
-        .catch(error => {
-            res.status(500).json({ message: 'Erro ao simular o jogo', error})
-        })
+        res.status(201).json({ message: 'Jogo simulado com sucesso!', gol_time1, gol_time2 })
+    })
 }

8e4141278921a7cbb55ba3ddb2270b1eea2cd289
IcaroWil <icarofaria11@gmail.com>
2024-07-19 16:21:04 -0300

    chore: configurando init.sql para o MySQL com as tabelas

--- init.sql
+++ init.sql
@@ -1,16 +1,20 @@
+CREATE DATABASE IF NOT EXISTS meu_campeonato;
+
+USE meu_campeonato;
+
 CREATE TABLE IF NOT EXISTS times (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    pontuacao INT DEFAULT 0
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  pontuacao INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS jogos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    time1_id INT NOT NULL,
-    time2_id INT NOT NULL,
-    gol_time1 INT NOT NULL,
-    gol_time2 INT NOT NULL,
-    fase VARCHAR(50) NOT NULL,
-    FOREIGN KEY (time1_id) REFERENCES times(id),
-    FOREIGN KEY (time2_id) REFERENCES times(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  time1_id INT,
+  time2_id INT,
+  gol_time1 INT,
+  gol_time2 INT,
+  fase VARCHAR(50),
+  FOREIGN KEY (time1_id) REFERENCES times(id),
+  FOREIGN KEY (time2_id) REFERENCES times(id)
 );

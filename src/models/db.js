a91f49281b99ccef5fda731b2c8bd52c1ce3300a
IcaroWil <icarofaria11@gmail.com>
2024-07-19 16:26:53 -0300

    chore: config das portas do MySQL

--- src/models/db.js
+++ src/models/db.js
@@ -3,7 +3,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'meu_campeonato'
+    database: process.env.DB_NAME || 'meu_campeonato',
+    port: process.env.DB_PORT || 3306
 })
 
 connection.connect((err) => {

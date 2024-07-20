09145b5407775c4eaa3c617eb1eda3e0621c1383
IcaroWil <icarofaria11@gmail.com>
2024-07-20 20:39:26 -0300

    feat: adicionando rotas para endpoints de campeonatos

--- src/routes/campeonatoRoutes.js
+++ src/routes/campeonatoRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const campeonatoController = require('../controllers/campeonatoController');
+const express = require('express')
+const campeonatoController = require('../controllers/campeonatoController')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/inserir-times', campeonatoController.inserirTimes);
-router.get('/chaveamento', campeonatoController.chaveamentoCampeonato);
-router.post('/simular-jogo', campeonatoController.simularJogo);
-router.get('/verificar-vencedores/:fase', campeonatoController.verificarVencedores);
-router.get('/chaveamento-semis', campeonatoController.chaveamentoSemis);
-router.get('/chaveamento-final-terceiro', campeonatoController.chaveamentoFinalTerceiro);
-router.get('/campeonatos-anteriores', campeonatoController.recuperarCampeonatosAnteriores);
+router.post('/inserir-times', campeonatoController.inserirTimes)
+router.get('/chaveamento', campeonatoController.chaveamentoCampeonato)
+router.post('/simular-jogo', campeonatoController.simularJogo)
+router.get('/verificar-vencedores/:fase', campeonatoController.verificarVencedores)
+router.get('/chaveamento-semis', campeonatoController.chaveamentoSemis)
+router.get('/chaveamento-final-terceiro', campeonatoController.chaveamentoFinalTerceiro)
+router.get('/campeonatos-anteriores', campeonatoController.recuperarCampeonatosAnteriores)
 
-module.exports = router;
+module.exports = router

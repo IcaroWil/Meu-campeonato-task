025ff37cc8658f4db3eeb349caecf8855f39c727
IcaroWil <icarofaria11@gmail.com>
2024-07-20 20:41:04 -0300

    feat: add utilitário para gerenciar o banco de dados

--- src/utils/dbUtils.js
+++ src/utils/dbUtils.js
@@ -1,20 +1,20 @@
-const createConnection = require('../models/db');
+const createConnection = require('../models/db')
 
 async function waitForDatabase() {
     while (true) {
         try {
-            const connection = await createConnection();
-            console.log('Banco de dados conectado com sucesso!');
-            connection.end();
-            break;
+            const connection = await createConnection()
+            console.log('Banco de dados conectado com sucesso!')
+            connection.end()
+            break
         } catch (err) {
-            console.error('Erro ao conectar ao banco de dados:', err.message);
-            console.log('Banco de dados não está pronto. Tentando novamente...');
-            await new Promise(resolve => setTimeout(resolve, 10000));
+            console.error('Erro ao conectar ao banco de dados:', err.message)
+            console.log('Banco de dados não está pronto. Tentando novamente...')
+            await new Promise(resolve => setTimeout(resolve, 10000))
         }
     }
 }
 
 module.exports = {
     waitForDatabase
-};
+}

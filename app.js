8eb1740c8b27afa4be5f38d273edc574f039b6ca
IcaroWil <icarofaria11@gmail.com>
2024-07-20 15:06:00 -0300

    chore: config do banco e servidor

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const createConnection = require('./src/models/db');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const createConnection = require('./src/models/db')
 
 async function waitForDatabase() {
     while (true) {
         try {
-            const connection = await createConnection();
-            console.log('Banco de dados conectado com sucesso!');
-            connection.end();
-            break;
+            const connection = await createConnection()
+            console.log('Banco de dados conectado com sucesso!')
+            connection.end()
+            break
         } catch (err) {
-            console.error('Erro ao conectar ao banco de dados:', err.message);
-            console.log('Banco de dados não está pronto. Tentando novamente...');
-            await new Promise(resolve => setTimeout(resolve, 10000));
+            console.error('Erro ao conectar ao banco de dados:', err.message)
+            console.log('Banco de dados não está pronto. Tentando novamente...')
+            await new Promise(resolve => setTimeout(resolve, 10000))
         }
     }
 }
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-const campeonatoRoutes = require('./src/routes/campeonatoRoutes');
-app.use('/api/campeonato', campeonatoRoutes);
+const campeonatoRoutes = require('./src/routes/campeonatoRoutes')
+app.use('/api/campeonato', campeonatoRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 waitForDatabase().then(() => {
     app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
-    });
-});
+        console.log(`Servidor rodando na porta ${PORT}`)
+    })
+})

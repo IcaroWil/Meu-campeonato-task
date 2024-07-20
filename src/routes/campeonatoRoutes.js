50a293752685c46662f230d5f71fa85a6c8d11db
IcaroWil <icarofaria11@gmail.com>
2024-07-20 15:06:40 -0300

    fix: configurando as rotas da API

--- src/routes/campeonatoRoutes.js
+++ src/routes/campeonatoRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const campeonatoController = require('../controllers/campeonatoController');
+const express = require('express')
+const router = express.Router()
+const campeonatoController = require('../controllers/campeonatoController')
 
-router.post('/inserir-times', campeonatoController.inserirTimes);
-router.get('/chaveamento', campeonatoController.chaveamentoCampeonato);
-router.post('/simular-jogo', campeonatoController.simularJogo);
-router.get('/verificar-vencedores/:fase', campeonatoController.verificarVencedores);
-router.get('/chaveamento-semis', campeonatoController.chaveamentoSemis);
-router.get('/chaveamento-final-terceiro', campeonatoController.chaveamentoFinalTerceiro);
+router.post('/inserir-times', campeonatoController.inserirTimes)
+router.get('/chaveamento', campeonatoController.chaveamentoCampeonato)
+router.post('/simular-jogo', campeonatoController.simularJogo)
+router.get('/verificar-vencedores/:fase', campeonatoController.verificarVencedores)
+router.get('/chaveamento-semis', campeonatoController.chaveamentoSemis)
+router.get('/chaveamento-final-terceiro', campeonatoController.chaveamentoFinalTerceiro)
 
-module.exports = router;
+module.exports = router
